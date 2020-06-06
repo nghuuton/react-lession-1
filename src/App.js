57dff236b289f8e4d3276d129ba34f4a58fb6083
Nguyen Huu Ton <nghuuton@gmail.com>
2020-06-06 14:06:37 +0700

    Delete Movie

--- src/App.js
+++ src/App.js
@@ -1,63 +1,66 @@
-import React, { Component } from 'react';
-// import './App.css';
-import { getMovies } from './services/fakeMovieService'
+import React, { Component } from "react";
+// import "./App.css";
+import { getMovies } from "./services/fakeMovieService";
 // import Counter from './components/counter'
-
-
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       movies: getMovies(),
-      count: getMovies().length
-    }
+      count: getMovies().length,
+    };
   }
-  deleteMovie(movieId) {
-    return () => {
-      const { movies, count } = this.state;
-      let newCout = count - 1;
-      const newData = movies.filter(item => {
-        return item._id !== movieId
-      })
-      this.setState({
-        movies: newData,
-        count: newCout
-      })
-    }
-  }
+  deleteMovie = (movieId) => {
+    const movies = this.state.movies.filter((movie) => movie._id !== movieId);
+    this.setState({
+      movies: movies,
+      count: this.state.count - 1,
+    });
+  };
   render() {
     const { movies, count } = this.state;
+    if (count === 0) return <span>There are no movies in the databases.</span>;
     return (
-      <div className="contrainer" >
-        {/* <Counter /> */}
-        {count > 0 && <span>Showing {count} movies in the database.</span>}
-        {count === 0 && <span>There are no movies in the database</span>}
-        {count > 0 && <div className="row">
-          <div className="col">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Title</th>
-                  <th scope="col">Genre</th>
-                  <th scope="col">Stock</th>
-                  <th scope="col">Rate</th>
-                  <th scope="col">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {movies.map((movie, index) => (<tr key={index}>
-                  <th scope="row">{movie.title}</th>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td><button className="btn btn-danger" onClick={this.deleteMovie(movie._id)}>Delete</button></td>
-                </tr>))}
-              </tbody>
-            </table>
+      <React.Fragment>
+        <div className="contrainer">
+          {/* <Counter /> */}
+          <p>Showing {count} movies in the databases.</p>
+          <div className="row">
+            <div className="col">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Genre</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Rate</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {movies.map((movie, index) => (
+                    <tr key={index}>
+                      <th scope="row">{movie.title}</th>
+                      <td>{movie.genre.name}</td>
+                      <td>{movie.numberInStock}</td>
+                      <td>{movie.dailyRentalRate}</td>
+                      <td>
+                        <button
+                          className="btn btn-outline-danger"
+                          onClick={() => this.deleteMovie(movie._id)}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>}
-      </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,45 +1,48 @@
-import React, { Component } from 'react';
-import classNames from 'classnames'
+import React, { Component } from "react";
+import classNames from "classnames";
 class Counter extends Component {
-	state = {
-		count: 0,
-		tags: ['tag1', 'tag2', 'tag3']
-	};
-	formatCount() {
-		const { count } = this.state;
-		return count === 0 ? 'Zero' : count;
-	};
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 
-	increment(productId) {
-		return () => {
-			this.setState({
-				count: this.state.count + productId
-			})
-		}
+  increment(productId) {
+    return () => {
+      this.setState({
+        count: this.state.count + productId,
+      });
+    };
+  }
 
-	};
-
-
-	render() {
-		const { count, tags } = this.state;
-		return (
-			<div className="counter">
-				<span
-					className={classNames('badge badge-primary mr-2', { 'badge-warning': count === 0 })}>
-					{this.formatCount()}
-				</span>
-				<button
-					className="btn btn-secondary primary btn-sm"
-					onClick={this.increment(1)}>
-					Increment
-                </button>
-				<ul>
-					{tags.length > 0 && tags.map((item, index) => <li key={index}>{item}</li>)}
-					{tags.length === 0 && <li>Nothing in here!</li>}
-				</ul>
-			</div>
-		);
-	}
+  render() {
+    const { count, tags } = this.state;
+    return (
+      <div className="counter">
+        <span
+          className={classNames("badge badge-primary mr-2", {
+            "badge-warning": count === 0,
+          })}
+        >
+          {this.formatCount()}
+        </span>
+        <button
+          className="btn btn-secondary primary btn-sm"
+          onClick={this.increment(1)}
+        >
+          Increment
+        </button>
+        <ul>
+          {tags.length > 0 &&
+            tags.map((item, index) => <li key={index}>{item}</li>)}
+          {tags.length === 0 && <li>Nothing in here!</li>}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Counter;
