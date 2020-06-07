3fda0ea4643a28d307256866b907c42e4bed49db
Nguyen Huu Ton <nghuuton@gmail.com>
2020-06-07 18:21:23 +0700

    Updates

---
+++ precache-manifest.11aab4b6ec43be40a45ea9d963c8a6f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a23a136ac230b5505d72437eb1c7c3",
+    "url": "/react-lession-1/index.html"
+  },
+  {
+    "revision": "63b2b1089d290602d2eb",
+    "url": "/react-lession-1/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "10f197c4abe9e53f2db6",
+    "url": "/react-lession-1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "63b2b1089d290602d2eb",
+    "url": "/react-lession-1/static/js/2.23d7683d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-lession-1/static/js/2.23d7683d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f197c4abe9e53f2db6",
+    "url": "/react-lession-1/static/js/main.acd32448.chunk.js"
+  },
+  {
+    "revision": "d0f247bfae03c8d9f900",
+    "url": "/react-lession-1/static/js/runtime-main.e5d79d8b.js"
+  }
+]);
